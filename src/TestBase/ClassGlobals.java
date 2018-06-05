d0d45cda63c6c014b40309a77035447f1ea67f02
harvey-fletcher <harvey.fletcher1@ntlworld.com>
2018-06-05 11:36:37 +0100

    Single testbase package

---
+++ src/TestBase/ClassGlobals.java
@@ -0,0 +1,7 @@
+package TestBase;
+
+public class ClassGlobals extends WebDriverSetup{
+
+    //This is where the selenium hub is located.
+    public static String hubUrl = "http://10.168.206.3:4445/wd/hub";
+}
