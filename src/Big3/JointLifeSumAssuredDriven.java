413028bd4bb49644211d8cdc74faa06819c1f0f4
harvey-fletcher <harvey.fletcher1@ntlworld.com>
2018-06-14 14:24:22 +0100

    Test to check the quoteresponses when we are quoting for a Big 3 CIC joint policy

--- src/Big3/JointLifeSumAssuredDriven.java
+++ src/Big3/JointLifeSumAssuredDriven.java
@@ -18,7 +18,7 @@ import java.util.Date;
 /**
  * Created by hfletcher on 14/06/2018.
  */
-public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
+public class JointLifeSumAssuredDriven extends TestBase.ClassGlobals {
 
     @Test
     public void main(){
@@ -83,8 +83,6 @@ public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
 
             System.out.println("Client confirmed");
 
-            /* Define dropdowns and web elements */
-            Select drpSmoker = new Select(driver().findElement(By.xpath("//*[@id=\"smoker_1\"]")));
             Select drpLives = new Select(driver().findElement(By.xpath("//*[@id=\"life_covered\"]")));
             Select drpQuote = new Select(driver().findElement(By.xpath("//*[@id=\"quotation_basis\"]")));
             Select drpCIC = new Select(driver().findElement(By.xpath("//*[@id=\"cic\"]")));
@@ -93,7 +91,6 @@ public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
             Select drpDeath = new Select(driver().findElement(By.xpath("//*[@id=\"death\"]")));
             Select drpFrequency = new Select(driver().findElement(By.xpath("//*[@id=\"payment_frequency\"]")));
 
-            drpSmoker.selectByVisibleText("No");
             drpLives.selectByIndex(0);
             drpQuote.selectByVisibleText("Sum");
             drpCIC.selectByVisibleText("No");
@@ -102,27 +99,44 @@ public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
             drpDeath.selectByVisibleText("1st");
             drpFrequency.selectByVisibleText("Month");
 
-            System.out.println("Life panel quotes selected.");
+            for (int i = 1; i<3; i++){
+                /* Define dropdowns and web elements */
+                Select drpSmoker = new Select(driver().findElement(By.xpath("//*[@id=\"smoker_"+ i +"\"]")));
+                drpSmoker.selectByVisibleText("No");
+                System.out.println("Client " + i + " smoker status set");
 
-            //Set up the client first name
-            driver().findElement(By.xpath("//*[@id=\"forename_1\"]")).clear();
-            driver().findElement(By.xpath("//*[@id=\"forename_1\"]")).sendKeys("Tester");
-            System.out.println("Client first name cleared and then set.");
+                //Set the sex field
+                Select drpSex = new Select(driver().findElement(By.xpath("//*[@id=\"gender_"+ i +"\"]")));
+                drpSex.selectByVisibleText("Male");
+                System.out.println("Client " + i + " sex set to male");
 
-            //Set up the client surname
-            driver().findElement(By.xpath("//*[@id=\"surname_1\"]")).clear();
-            driver().findElement(By.xpath("//*[@id=\"surname_1\"]")).sendKeys("Testeez");
-            System.out.println("Client last name cleared and then set.");
+                //Set the title for each client
+                driver().findElement(By.xpath("//*[@id=\"title_"+ i +"\"]")).clear();
+                driver().findElement(By.xpath("//*[@id=\"title_"+ i +"\"]")).sendKeys("Mr");
+                System.out.println("Client " + i + " title cleared and then set.");
+
+                //Set up the client first name
+                driver().findElement(By.xpath("//*[@id=\"forename_"+ i +"\"]")).clear();
+                driver().findElement(By.xpath("//*[@id=\"forename_"+ i +"\"]")).sendKeys("Tester");
+                System.out.println("Client " + i + " first name cleared and then set.");
+
+                //Set up the client surname
+                driver().findElement(By.xpath("//*[@id=\"surname_"+ i + "\"]")).clear();
+                driver().findElement(By.xpath("//*[@id=\"surname_"+ i + "\"]")).sendKeys("Testeez");
+                System.out.println("Client " + i + " last name cleared and then set.");
+
+                //Set the date of birth
+                driver().findElement(By.xpath("//*[@id=\"dob_"+ i +"\"]")).clear();
+                driver().findElement(By.xpath("//*[@id=\"dob_"+ i +"\"]")).sendKeys(methods.DOBFromAge(25));
+                System.out.println("Client " + i + " DOB cleared and then set");
+            }
 
             //Set up the life sum assured.
             driver().findElement(By.xpath("//*[@id=\"sum_assured\"]")).clear();
             driver().findElement(By.xpath("//*[@id=\"sum_assured\"]")).sendKeys("100000");
             System.out.println("Life Sum Assured cleared and then set.");
 
-            //Set the date of birth
-            driver().findElement(By.xpath("//*[@id=\"dob_1\"]")).clear();
-            driver().findElement(By.xpath("//*[@id=\"dob_1\"]")).sendKeys(methods.DOBFromAge(25));
-            System.out.println("Client 1 DOB cleared and then set");
+            System.out.println("Life panel quotes selected.");
 
             //Set the policy term
             driver().findElement(By.xpath("//*[@id=\"term\"]")).clear();
@@ -179,20 +193,29 @@ public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
             WebElement EligibilityForenameOne = driver().findElement(By.xpath("//*[@id=\"forename_1\"]"));
             WebElement EligibilitySurnameOne = driver().findElement(By.xpath("//*[@id=\"surname_1\"]"));
 
-            String[][] SumAssuredCases = QuoteGraph.SingleLifeSumAssured;
+            WebElement EligibilityTitleTwo = driver().findElement(By.xpath("//*[@id=\"title_2\"]"));
+            WebElement EligibilityForenameTwo = driver().findElement(By.xpath("//*[@id=\"forename_2\"]"));
+            WebElement EligibilitySurnameTwo = driver().findElement(By.xpath("//*[@id=\"surname_2\"]"));
+
+            String[][] SumAssuredCases = QuoteGraph.JointLifeSumAssured;
 
             for(int i=0; i<SumAssuredCases.length; i++){
             //for(int i=0; i<1; i++){
 
-                //Extract all the required values from the array
-                int AgeNextBirthDay = Integer.parseInt(SumAssuredCases[i][1]);
-                String SmokerStatus = SumAssuredCases[i][2];
+                //Extract general policy details from the array
                 String SumAssured = SumAssuredCases[i][5].replace(",","");
                 String PolicyTerm = SumAssuredCases[i][6];
                 String ExpectedPremium = SumAssuredCases[i][7];
-                String ExpectedCommission = SumAssuredCases[i][8];
 
-                //We are going to use a pre set name, as names do not affect the premium.
+                //Extract all the required values from the array for customer 1
+                int C1AgeNextBirthDay = Integer.parseInt(SumAssuredCases[i][1]);
+                String C1SmokerStatus = SumAssuredCases[i][2];
+
+                //Extract all the required values from the array for customer 2
+                int C2AgeNextBirthDay = Integer.parseInt(SumAssuredCases[i][3]);
+                String C2SmokerStatus = SumAssuredCases[i][4];
+
+                //We are going to use a pre set name for customer 1, as names do not affect the premium.
                 EligibilityTitleOne.clear();
                 EligibilityTitleOne.sendKeys("Mr");
                 System.out.println("Set customer 1 title");
@@ -203,14 +226,30 @@ public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
                 EligibilitySurnameOne.sendKeys("Testeez");
                 System.out.println("Set customer 1 surname");
 
+                //We are going to use a pre set name for customer 2, as names do not affect the premium.
+                EligibilityTitleTwo.clear();
+                EligibilityTitleTwo.sendKeys("Mrs");
+                System.out.println("Set customer 2 title");
+                EligibilityForenameTwo.clear();
+                EligibilityForenameTwo.sendKeys("Testet");
+                System.out.println("Set customer 2 forename");
+                EligibilitySurnameTwo.clear();
+                EligibilitySurnameTwo.sendKeys("Testeez");
+                System.out.println("Set customer 2 surname");
+
                 //Now we are going to set the sex to be male. This does not affect the premium.
                 Select clientOneSexSelect = new Select(driver().findElement(By.xpath("//*[@id=\"gender_1\"]")));
                 clientOneSexSelect.selectByVisibleText("Male");
                 System.out.println("Set customer 1 to male");
 
-                //Now we need to set the smoker status from the value in the customer profile
+                //Now we are going to set the sex to be female. This does not affect the premium.
+                Select clientTwoSexSelect = new Select(driver().findElement(By.xpath("//*[@id=\"gender_2\"]")));
+                clientTwoSexSelect.selectByVisibleText("Female");
+                System.out.println("Set customer 2 to female");
+
+                //Now we need to set the smoker status from the value in the customer profile for customer 1
                 Select clientOneSmokerStat = new Select(driver().findElement(By.xpath("//*[@id=\"smoker_1\"]")));
-                if(SmokerStatus.matches("Smoker")) {
+                if(C1SmokerStatus.matches("Smoker")) {
                     clientOneSmokerStat.selectByVisibleText("Yes");
                     System.out.println("Customer 1 is a smoker.");
                 } else {
@@ -218,11 +257,27 @@ public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
                     System.out.println("Customer 1 is not a smoker.");
                 }
 
-                //Now we need to calculate the date of birth
-                String customerOneDob = methods.DOBFromAge(AgeNextBirthDay - 1);
+                //Now we need to set the smoker status from the value in the customer profile for customer 2
+                Select clientTwoSmokerStat = new Select(driver().findElement(By.xpath("//*[@id=\"smoker_2\"]")));
+                if(C2SmokerStatus.matches("Smoker")) {
+                    clientTwoSmokerStat.selectByVisibleText("Yes");
+                    System.out.println("Customer 2 is a smoker.");
+                } else {
+                    clientTwoSmokerStat.selectByVisibleText("No");
+                    System.out.println("Customer 2 is not a smoker.");
+                }
+
+                //Now we need to calculate the date of birth for customer 1
+                String customerOneDob = methods.DOBFromAge(C1AgeNextBirthDay - 1);
                 driver().findElement(By.xpath("//*[@id=\"dob_1\"]")).clear();
                 driver().findElement(By.xpath("//*[@id=\"dob_1\"]")).sendKeys(customerOneDob);
                 System.out.println("Customer 1 date of birth set to " + customerOneDob);
+
+                //Now we need to calculate the date of birth for customer 2
+                String customerTwoDob = methods.DOBFromAge(C2AgeNextBirthDay - 1);
+                driver().findElement(By.xpath("//*[@id=\"dob_2\"]")).clear();
+                driver().findElement(By.xpath("//*[@id=\"dob_2\"]")).sendKeys(customerTwoDob);
+                System.out.println("Customer 2 date of birth set to " + customerTwoDob);
 
                 //Select the update client details
                 driver().findElement(By.xpath("//*[@id=\"update_lead\"]")).click();
@@ -248,24 +303,45 @@ public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
                     throw new ElementNotVisibleException("");
                 }
 
+                //Check to see if the buttons for customer 1 are displayed
+                boolean IsCustomer1QuestionPresent = false;
+                try{
+                    IsCustomer1QuestionPresent = driver().findElements(By.xpath("//*[@id=\"client_1_answers\"]")).size() > 0;
+                } catch (NoSuchElementException e){
+                    System.out.println("The eligibility questions for client 1 were not displayed - bad");
+                }
+                if(!IsCustomer1QuestionPresent){
+                    System.out.println("TEST FAILED - CUSTOMER 1 ELIGIBILITY QUESTIONS NOT DISPLAYED WHEN THEY SHOULD HAVE BEEN");
+                    throw new Exception("Elements NOT displayed when they should have been.");
+                }
+
                 //Check to see if the buttons for customer 2 are displayed
                 boolean IsCustomer2QuestionPresent = false;
                 try{
                     IsCustomer2QuestionPresent = driver().findElements(By.xpath("//*[@id=\"client_2_answers\"]")).size() > 0;
                 } catch (NoSuchElementException e){
-                    System.out.println("The eligibility questions for client 2 were not displayed - Good");
+                    System.out.println("The eligibility questions for client 2 were not displayed - bad");
                 }
-                if(IsCustomer2QuestionPresent){
-                    System.out.println("TEST FAILED - CUSTOMER 2 ELIGIBILITY QUESTIONS DISPLAYED WHEN THEY SHOULD NOT HAVE BEEN");
-                    throw new Exception("Elements displayed when they should not have been.");
+                if(!IsCustomer2QuestionPresent){
+                    System.out.println("TEST FAILED - CUSTOMER 2 ELIGIBILITY QUESTIONS NOT DISPLAYED WHEN THEY SHOULD HAVE BEEN");
+                    throw new Exception("Elements NOT displayed when they should have been.");
                 }
 
-                //Set the eligibility options
+                //Set the eligibility options for client 1
                 driver().findElement(By.xpath("//*[@id=\"client_1_answers\"]/p[1]/span[1]/label/input")).click();
                 driver().findElement(By.xpath("//*[@id=\"client_1_answers\"]/p[2]/span[1]/label/input")).click();
                 driver().findElement(By.xpath("//*[@id=\"client_1_answers\"]/p[3]/span[2]/label/input")).click();
                 driver().findElement(By.xpath("//*[@id=\"client_1_answers\"]/p[4]/span[2]/label/input")).click();
                 driver().findElement(By.xpath("//*[@id=\"client_1_answers\"]/p[5]/span[2]/label/input")).click();
+                System.out.println("Customer 1 eligibility answered.");
+
+                //Set the eligibility options for client 2
+                driver().findElement(By.xpath("//*[@id=\"client_2_answers\"]/p[1]/span[1]/label/input")).click();
+                driver().findElement(By.xpath("//*[@id=\"client_2_answers\"]/p[2]/span[1]/label/input")).click();
+                driver().findElement(By.xpath("//*[@id=\"client_2_answers\"]/p[3]/span[2]/label/input")).click();
+                driver().findElement(By.xpath("//*[@id=\"client_2_answers\"]/p[4]/span[2]/label/input")).click();
+                driver().findElement(By.xpath("//*[@id=\"client_2_answers\"]/p[5]/span[2]/label/input")).click();
+                System.out.println("Customer 2 eligibility answered.");
 
                 //Click to confirm eligibility
                 driver().findElement(By.xpath("//*[@id=\"btn_validate_qs\"]")).click();
@@ -281,8 +357,8 @@ public class SingleLifeSumAssuredDriven extends TestBase.ClassGlobals{
                 System.out.println("Switched to Big3 Quoting Tab");
 
                 //Select the single life radial button.
-                driver().findElement(By.xpath("//*[@id=\"quote_details_single\"]/p[2]/input")).click();
-                System.out.println("Selected single life 1 radial");
+                driver().findElement(By.xpath("//*[@id=\"quote_details_joint_wrapper\"]/p[2]/span/input")).click();
+                System.out.println("Selected joint life radial");
 
                 //Enter the term
                 driver().findElement(By.xpath("//*[@id=\"quote_term\"]")).clear();
--- src/Big3/QuoteGraph.java
+++ src/Big3/QuoteGraph.java
@@ -23,4 +23,15 @@ public class QuoteGraph{
             {"Single", "49", "NonSmoker", "N/A", "N/A", "13,000", "9", "£9.34", "£232.01"}
     };
 
+    public static String[][] JointLifeSumAssured = new String[][]{
+            {"Joint", "50", "NonSmoker", "50", "NonSmoker", "10,000", "10", "£15.54", "£428.90"},
+            {"Joint", "19", "Smoker", "19", "NonSmoker", "45,678", "15", "£11.14", "£245.97"},
+            {"Joint", "19", "Smoker", "50", "Smoker", "33,000", "10", "£35.01", "£483.14"},
+            {"Joint", "27", "NonSmoker", "50", "NonSmoker", "99,000", "5", "£76.98", "£1,062.32"},
+            {"Joint", "27", "NonSmoker", "50", "NonSmoker", "67,777", "9", "£60.20", "£1,495.37"},
+            {"Joint", "50", "Smoker_Infrequent", "50", "NonSmoker", "100,000", "10", "£173.60", "£4,791.36"},
+            {"Joint", "27", "Smoker_Infrequent", "27", "Smoker_Infrequent", "99,999", "5", "£28.90", "£398.82"},
+            {"Joint", "33", "Smoker", "33", "Smoker_Infrequent", "1,000", "10", "£0.46", "£12.70"}
+    };
+
 }
