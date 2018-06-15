96aa22e23c7a4d53e400983cc7716d92fbe84e02
harvey-fletcher <harvey.fletcher1@ntlworld.com>
2018-06-15 11:58:23 +0100

    Can now quote joint life for big 3 by premium

--- src/Big3/QuoteGraph.java
+++ src/Big3/QuoteGraph.java
@@ -5,7 +5,7 @@ public class QuoteGraph{
     public static String[][] SingleLifeSumAssured = new String[][]{
             {"Single", "50", "NonSmoker", "N/A", "N/A", "10,000", "10", "£8.17", "£225.49"},
             {"Single", "19", "Smoker", "N/A", "N/A", "49,000", "5", "£8.05", "£111.09"},
-            {"Single", "23", "Smoker_Infrequent", "N/A", "N/A", "90,000", "15", "£15.98", "£441.05"},
+            {"Single", "23", "Smoker", "N/A", "N/A", "90,000", "15", "£15.98", "£441.05"},  //Smoker Infrequent
             {"Single", "29", "NonSmoker", "N/A", "N/A", "89,000", "10", "£10.99", "£0.00"},
             {"Single", "28", "NonSmoker", "N/A", "N/A", "75,000", "11", "£13.82", "£381.43"},
             {"Single", "31", "Smoker", "N/A", "N/A", "75,000", "12", "£18.28", "£504.53"},
@@ -16,7 +16,7 @@ public class QuoteGraph{
             {"Single", "30", "NonSmoker", "N/A", "N/A", "89,768", "30", "£22.76", "£628.18"},
             {"Single", "27", "Smoker", "N/A", "N/A", "100,000", "10", "£18.44", "£508.94"},
             {"Single", "35", "NonSmoker", "N/A", "N/A", "50,000", "10", "£12.88", "£355.49"},
-            {"Single", "30", "Smoker_Infrequent", "N/A", "N/A", "49,000", "10", "£11.40", "£314.64"},
+            {"Single", "30", "Smoker", "N/A", "N/A", "49,000", "10", "£11.40", "£314.64"}, //Smoker Infrequent
             {"Single", "28", "NonSmoker", "N/A", "N/A", "100,000", "10", "£16.73", "£457.13"},
             {"Single", "50", "NonSmoker", "N/A", "N/A", "13,000", "9", "£10.54", "£261.81"},
             {"Single", "50", "NonSmoker", "N/A", "N/A", "13,000", "9", "£10.54", "£261.81"},
@@ -29,9 +29,9 @@ public class QuoteGraph{
             {"Joint", "19", "Smoker", "50", "Smoker", "33,000", "10", "£35.01", "£483.14"},
             {"Joint", "27", "NonSmoker", "50", "NonSmoker", "99,000", "5", "£76.98", "£1,062.32"},
             {"Joint", "27", "NonSmoker", "50", "NonSmoker", "67,777", "9", "£60.20", "£1,495.37"},
-            {"Joint", "50", "Smoker_Infrequent", "50", "NonSmoker", "100,000", "10", "£173.60", "£4,791.36"},
-            {"Joint", "27", "Smoker_Infrequent", "27", "Smoker_Infrequent", "99,999", "5", "£28.90", "£398.82"},
-            {"Joint", "33", "Smoker", "33", "Smoker_Infrequent", "1,000", "10", "£0.46", "£12.70"}
+            {"Joint", "50", "Smoker", "50", "NonSmoker", "100,000", "10", "£173.60", "£4,791.36"}, //Smoker Infrequent client 1 only
+            {"Joint", "27", "Smoker", "27", "Smoker", "99,999", "5", "£28.90", "£398.82"}, //Smoker Infrequent client 1 and 2
+            {"Joint", "33", "Smoker", "33", "Smoker", "1,000", "10", "£0.46", "£12.70"} //Smoker Infrequent client 2 only
     };
 
     public static String[][] SingleLifeQuoteByPremium = new String[][]{
@@ -40,9 +40,9 @@ public class QuoteGraph{
     };
 
     public static String[][] JointLifeQuoteByPremium = new String[][]{
-            {"Joint", "41", "NonSmoker", "19", "Smoker_Infrequent", "", "6", "£4.00", "£66.24"},
-            {"Joint", "47", "NonSmoker", "41", "Smoker", "", "7", "£77.00", "£1,487.64"},
-            {"Joint", "41", "Smoker", "41", "Smoker_Infrequent", "", "11", "£9.98", "£275.45"}
+            {"Joint", "41", "NonSmoker", "19", "Smoker_Infrequent", "9,265", "6", "£4.00", "£66.24"}, //Smoker Infrequent client 2
+            {"Joint", "47", "NonSmoker", "41", "Smoker", "88,457", "7", "£77.00", "£1,487.64"},
+            {"Joint", "41", "Smoker", "41", "Smoker", "11,914", "11", "£9.98", "£275.45"}
     };
 
 }
