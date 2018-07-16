b2f78ac8c920786db28d32a510406fa9a695e142
harvey-fletcher <harvey.fletcher1@ntlworld.com>
2018-07-16 11:38:08 +0100

    added a common method for customer details addition, test the contact preferences options on customer 1 and 2 in both directions

--- src/RegressionTests/GDPRRegression/SalesUsersGDPR_DEV_T1453.java
+++ src/RegressionTests/GDPRRegression/SalesUsersGDPR_DEV_T1453.java
@@ -1,14 +1,17 @@
 package RegressionTests.GDPRRegression;
 
+import TestBase.CommonMethods;
 import TestBase.WebDriverSetup;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -91,13 +94,160 @@ public class SalesUsersGDPR_DEV_T1453 extends TestBase.ClassGlobals{
                 throw new Exception("TestFailedException");
             }
 
-            //Now, observe each button individually to see what colour it is
-            String[] buttonColours = new String[]{};
-            for(int i=1;i<5;i++){
-                String buttonColour = driver.findElement(By.xpath("//*[@id=\"container-customer-details-1\"]/table[1]/tbody/tr[1]/td[1]/button["+ i +"]")).getAttribute("class") ;
-                com.log(logFile, buttonColour);
-            }
+            //An array of the letters on each button
+            String[] buttonLetters = new String[]{
+                    "T",
+                    "M",
+                    "E",
+                    "S"
+            };
 
+            for(int r=0; r<2;r++) {
+
+                //Now, observe each button individually to see what colour it is
+                String[] buttonColours = new String[]{};
+                for (int i = 1; i < 5; i++) {
+                    //Store the button as an element
+                    WebElement button = driver.findElement(By.xpath("//*[@id=\"container-customer-details-1\"]/table[1]/tbody/tr[1]/td[1]/button[" + i + "]"));
+
+                    //Get the class names of the button.
+                    String oldButtonColour = button.getAttribute("class");
+
+                    //Split the class items into different names based on space
+                    String[] classItems = oldButtonColour.split(" ");
+
+                    //The color is the second item
+                    oldButtonColour = classItems[1];
+
+                    //Click the button
+                    button.click();
+
+                    //Wait for save completion
+                    Thread.sleep(500);
+
+                    //Log the update
+                    com.log(logFile, "Under customer 1, click [" + buttonLetters[i - 1] + "] -> The colour of the button updates.");
+
+                    //Refresh the page
+                    driver.get(driver.getCurrentUrl());
+
+                    //Log refresh
+                    com.log(logFile, "Refreshed the page");
+
+                    //Store the button as an element
+                    button = driver.findElement(By.xpath("//*[@id=\"container-customer-details-1\"]/table[1]/tbody/tr[1]/td[1]/button[" + i + "]"));
+
+                    //Get the class names of the button.
+                    String newButtonColour = button.getAttribute("class");
+
+                    //Split the class items into different names based on space
+                    classItems = newButtonColour.split(" ");
+
+                    //The color is the second item
+                    newButtonColour = classItems[1];
+
+                    //Check to see the button is the opposite colour.
+                    if (oldButtonColour.matches("green")) {
+                        if (newButtonColour.matches("red")) {
+                            com.log(logFile, "PASS: The button at position " + i + " updated successfully.");
+                        } else {
+                            com.log(logFile, "FAIL: The button at position " + i + " failed to save successfully.");
+                        }
+                    } else if (oldButtonColour.matches("red")) {
+                        if (newButtonColour.matches("green")) {
+                            com.log(logFile, "PASS: The button at position " + i + " updated successfully.");
+                        } else {
+                            com.log(logFile, "FAIL: The button at position " + i + " failed to save successfully.");
+                        }
+                    } else {
+                        com.log(logFile, "FAIL: The button at position " + i + " failed to save successfully -> Style Inconsitencies.");
+                    }
+
+                    //Log the old and new colours side by side.
+                    com.log(logFile, "The button at position " + i + " has changed from " + oldButtonColour + " to " + newButtonColour);
+                }
+
+
+                /**
+                 * NOW WE ARE GOING TO RUN THE SAME TESTS AGAIN
+                 * BUT THIS TIME, THEY WILL BE FOR THE OPTIONS ON
+                 * THE CUSTOMER 2 HALF OF THE PAGE.
+                 */
+
+                //First though, we will need to add customer 2 details, but only if they aren't already there.
+                boolean isCustomer2 = driver.findElements(By.xpath("//*[@id=\"container-customer-details-2\"]/table/tbody/tr[1]/td[1]/button[1]")).size() > 0;
+
+                if (!isCustomer2) {
+
+                    String[] path = driver.getCurrentUrl().split("/");
+                    int leadID = Integer.parseInt(path[5]);
+                    com.PopulateClientDetails(driver, logFile, leadID, 2, "mrs", "testert", "testeez", 36, "No", "Female");
+                    com.log(logFile, "Added a second customer.");
+
+                }
+
+                for (int i = 1; i < 5; i++) {
+                    //Store the button as an element
+                    WebElement button = driver.findElement(By.xpath("//*[@id=\"container-customer-details-2\"]/table/tbody/tr[1]/td[1]/button[" + i + "]"));
+
+                    //Get the class names of the button.
+                    String oldButtonColour = button.getAttribute("class");
+
+                    //Split the class items into different names based on space
+                    String[] classItems = oldButtonColour.split(" ");
+
+                    //The color is the second item
+                    oldButtonColour = classItems[1];
+
+                    //Click the button
+                    button.click();
+
+                    //Wait for save completion
+                    Thread.sleep(500);
+
+                    //Log the update
+                    com.log(logFile, "Under customer 2, click [" + buttonLetters[i - 1] + "] -> The colour of the button updates.");
+
+                    //Refresh the page
+                    driver.get(driver.getCurrentUrl());
+
+                    //Log refresh
+                    com.log(logFile, "Refreshed the page");
+
+                    //Store the button as an element
+                    button = driver.findElement(By.xpath("//*[@id=\"container-customer-details-2\"]/table[1]/tbody/tr[1]/td[1]/button[" + i + "]"));
+
+                    //Get the class names of the button.
+                    String newButtonColour = button.getAttribute("class");
+
+                    //Split the class items into different names based on space
+                    classItems = newButtonColour.split(" ");
+
+                    //The color is the second item
+                    newButtonColour = classItems[1];
+
+                    //Check to see the button is the opposite colour.
+                    if (oldButtonColour.matches("green")) {
+                        if (newButtonColour.matches("red")) {
+                            com.log(logFile, "PASS: The button at position " + i + " updated successfully.");
+                        } else {
+                            com.log(logFile, "FAIL: The button at position " + i + " failed to save successfully.");
+                        }
+                    } else if (oldButtonColour.matches("red")) {
+                        if (newButtonColour.matches("green")) {
+                            com.log(logFile, "PASS: The button at position " + i + " updated successfully.");
+                        } else {
+                            com.log(logFile, "FAIL: The button at position " + i + " failed to save successfully.");
+                        }
+                    } else {
+                        com.log(logFile, "FAIL: The button at position " + i + " failed to save successfully -> Style Inconsitencies.");
+                    }
+
+                    //Log the old and new colours side by side.
+                    com.log(logFile, "The button at position " + i + " has changed from " + oldButtonColour + " to " + newButtonColour);
+                }
+
+            }
 
             com.log(logFile, "-----TEST FINISHED-----");
             com.log(logFile, "Test passed");
